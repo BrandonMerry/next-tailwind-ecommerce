deebef121fe86f081ab0e75483141210715863e4
brandon <brandon.l.merry@gmail.com>
2022-09-27 15:50:39 +0700

    listed products on shop page + custom css

---
+++ pages/shop.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data';
+
+export default function Store() {
+  return (
+    <Layout title="Shop">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
+}
