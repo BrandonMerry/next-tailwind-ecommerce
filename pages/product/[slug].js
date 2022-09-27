45fca06d3861f82ac5f474af5e8e6ca9934b4cbf
brandon <brandon.l.merry@gmail.com>
2022-09-28 00:13:59 +0700

    implemented a shopping cart page/ CRUD  functions

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,12 +8,14 @@ import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
   const { state, dispatch } = useContext(Store);
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Produt Not Found</div>;
   }
+
   const addToCartHandler = () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
@@ -24,6 +26,7 @@ export default function ProductScreen() {
     }
 
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
 
   return (
