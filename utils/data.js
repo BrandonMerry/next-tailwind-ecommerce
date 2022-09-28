d04d5bd9423e239c562d646ffd8d3d9d20e817b8
brandon <brandon.l.merry@gmail.com>
2022-09-28 15:26:51 +0700

    connected mongo db

--- utils/data.js
+++ utils/data.js
@@ -1,6 +1,22 @@
 // this file is a data object for product arrays
+import bcrypt from 'bcryptjs';
 
 const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: 'Free Shirt',
