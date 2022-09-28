3dd659d6e62f24c1de17c30852cb0abaa7b8958a
brandon <brandon.l.merry@gmail.com>
2022-09-28 17:01:01 +0700

    implemented login api - next-auth

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,47 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
+import { getError } from '../utils/error';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
 export default function LoginScreen() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/');
+    }
+  }, [router, session, redirect]);
+
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
   };
   return (
     <Layout title="Login">
       <form
-        clasName="mx-auto max-w-screen-md"
+        className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
         <h1 className="mb-4 text-xl">Login</h1>
@@ -24,10 +50,10 @@ export default function LoginScreen() {
           <input
             type="email"
             {...register('email', {
-              required: 'Please enter your email address',
+              required: 'Please enter email',
               pattern: {
                 value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                message: 'Please enter a valid email address ',
+                message: 'Please enter valid email',
               },
             })}
             className="w-full"
@@ -50,13 +76,16 @@ export default function LoginScreen() {
             id="password"
             autoFocus
           ></input>
-          <div className="mb-4">
-            <button className="primary-button">Login</button>
-          </div>
-          <div className="mb-4">
-            Don&apos;t have an account? &nbsp;
-            <Link href="register">Register</Link>
-          </div>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4 ">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4 ">
+          Don&apos;t have an account? &nbsp;
+          <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
         </div>
       </form>
     </Layout>
